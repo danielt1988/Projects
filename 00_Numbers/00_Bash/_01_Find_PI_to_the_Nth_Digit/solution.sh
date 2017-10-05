bbaefb90b664721c40bca972eeff157fc97e51f0
Daniel Tucker <dantuka01@gmail.com>
2017-10-04 22:42:35 -0700

    MP-BASH-01-Solution-2
    - incert if condition to limit user input to 1-1000.

--- 00_Numbers/00_Bash/_01_Find_PI_to_the_Nth_Digit/solution.sh
+++ 00_Numbers/00_Bash/_01_Find_PI_to_the_Nth_Digit/solution.sh
@@ -4,6 +4,13 @@
 #Description: Find PI to the Nth Digit - Enter a number and have the program generate PI up to that many decimal places. Keep a limit to how far the program will go.
 
 read -p "To what Nth would you like to print PI? :" input
-RESULT=$(echo "scale=$input; 4*a(1)" | bc -l)
 
-echo "PI = ${RESULT}";
+
+if [[ $input > 1000 ]]; then
+	echo "Your input cannot be greater than 1000. Please try again."
+	read -p "To what Nth would you like to print PI? :" input
+	else
+		RESULT=$(echo "scale=$input; 4*a(1)" | bc -l)
+		echo "PI = ${RESULT}";
+fi
+
